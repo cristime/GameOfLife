87a578427ed66daf0c7d362d32a3ffcd31c41c86
Cristime Cai <thecristime@outlook.com>
2021-08-28 05:08:45 +0800

    Use vscode and gdb to debug. Update README.md. Update main.cpp(Use std::map to optimize). Delete ConfigGenerator.

--- README.md
+++ README.md
@@ -4,23 +4,27 @@
 使用 C++ 编写的 Conway 生命游戏，可以自定义地图。
 
 ## 使用方法
-安装一个 C++ 编译器（支持 C++11），cmake（>= 2.6）
+安装一个 C++ 编译器（支持 C++11），cmake（>= 2.8.12），ninja-build
 ```bash
 git clone https://github.com/cristime/GameOfLife.git
 mkdir GameOfLife/build
 cd GameOfLife/build
-cmake ..
-make
+cmake .. -G Ninja   # 第二次运行时可省略
+ninja
 ```
+
+具体使用见命令行参数说明
 ```
 $ GameOfLife -?
-usage: GameOfLife [options] ...
+usage: ./GameOfLife --sleepTime=unsigned int [options] ... 
 options:
-  -l, --length    map length (int [=50])
-  -h, --height    map height (int [=50])
-  -t, --type      is rand map (bool [=1])
-  -c, --config    config file path(if type is 0) (string [=])
-  -?, --help      print this message
+  -l, --length       map length (int [=50])
+  -h, --height       map height (int [=50])
+  -t, --type         is rand map (bool [=1])
+  -c, --config       config file path(if rand map disabled) (string [=])
+      --useSleep     Use sleep instead of getchar (bool [=0])
+      --sleepTime    Sleep time(ms) (if useSleep option enabled) (unsigned int)
+  -?, --help         print this message
 ```
 
 ## 如何编写 Config 文件
@@ -30,5 +34,4 @@ options:
 <a href="https://github.com/tanakh/cmdline">tanakh/cmdline</a>
 
 ## TODO
-* 使用队列优化空间复杂度
-* 优化 Config Generator
+* ~~使用队列优化空间复杂度(已使用 STL map 优化)~~
--- main.cpp
+++ main.cpp
@@ -5,23 +5,39 @@
 #endif
 */
 
+#if defined (_WIN32)
+#define CLEARWINDOW system("cls")
+#define SLEEP(SLEEPTIME) Sleep(SLEEPTIME)
+#else
+#include <unistd.h>
+#define CLEARWINDOW system("clear")
+#define SLEEP(SLEEPTIME) usleep(SLEEPTIME * 1000)
+#endif
+
 #include "header.h"
 #include "cmdline.h" // 解析命令行参数
 
 #include <fstream>
+#include <map>
+#include <algorithm>
 #include <ctime>
 
 int n, m, iterNum = 0, aliveNum = 0;
-bool mp[MAXL][MAXL], tmp[MAXL][MAXL];
+// bool mp[MAXL][MAXL], tmp[MAXL][MAXL];
+std::map<std::pair<int, int>, bool> aliveCells; // 用 map 来存储活细胞，pair 来表示其横纵坐标
 std::string fileName;
 cmdline::parser argParser;
 
 int main(int argc, char **argv)
 {
+	std::ios::sync_with_stdio(false); // 加速 io
+
 	argParser.add<int>("length", 'l', "map length", false, 50);
 	argParser.add<int>("height", 'h', "map height", false, 50);
 	argParser.add<bool>("type", 't', "is rand map", false, true);
-	argParser.add<std::string>("config", 'c', "config file path(if type is 0)", false);
+	argParser.add<std::string>("config", 'c', "config file path(if rand map disabled)", false);
+	argParser.add<bool>("useSleep", '\0', "Use sleep instead of getchar", false);
+	argParser.add<unsigned>("sleepTime", '\0', "Sleep time(ms) (if useSleep option enabled)", 500);
 
 	argParser.parse_check(argc, argv);
 
@@ -30,8 +46,8 @@ int main(int argc, char **argv)
 	{
 		if (!aliveNum)
 			break;
-		Iteration();
 		PrintMap();
+		Iteration();
 	}
 
 	return 0;
@@ -55,7 +71,7 @@ void Init()
 		}
 		while (std::cin >> x >> y)
 		{
-			mp[x][y] = 1;
+			aliveCells[std::make_pair(x, y)] = 1;
 			++aliveNum;
 		}
 		fin.close();
@@ -65,55 +81,59 @@ void Init()
 		srand(time(NULL));
 
 		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= m; j++)
-			{
-				mp[i][j] = rand() * rand() % 2;
-				aliveNum += mp[i][j] ? 1 : 0;
+			for (int j = 1; j <= m; j++) {
+				aliveCells[std::make_pair(i, j)] = (rand() * rand() % 2) ? 1 : 0;
+				aliveNum += aliveCells[std::make_pair(i, j)] ? 1 : 0;
 			}
 	}
 }
 
-void CopyMap(bool src[MAXL][MAXL], bool dst[MAXL][MAXL])
-{
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
-			dst[i][j] = src[i][j];
-}
-
 void Iteration()
 {
+	std::map<std::pair<int, int>, bool> another;
+	another.clear();
 	++iterNum;
-	CopyMap(mp, tmp);
+	// CopyMap(mp, tmp);
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++)
 		{
 			unsigned nearAliveNum = 0;
 			for (int k = 0; k < 8; k++)
-				nearAliveNum += mp[i + dx[k]][j + dy[k]] ? 1 : 0;
+				nearAliveNum += (aliveCells[std::make_pair(i + dx[k], j + dy[k])] ? 1 : 0);
+			// 	nearAliveNum += mp[i + dx[k]][j + dy[k]] ? 1 : 0;
 			if (nearAliveNum == 3)
-				tmp[i][j] = 1;
-			else if (nearAliveNum == 2 && mp[i][j])
-				tmp[i][j] = 1;
+				another[std::make_pair(i, j)] = 1;
+			else if (nearAliveNum == 2 && aliveCells[std::make_pair(i, j)])
+				another[std::make_pair(i, j)] = 1;
 			else
-				tmp[i][j] = 0;
+				another[std::make_pair(i, j)] = 0;
 		}
-	CopyMap(tmp, mp);
+	aliveCells.clear();
+	aliveCells = another;
+	// CopyMap(tmp, mp);
 	aliveNum = 0;
 	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			++aliveNum;
+		for (int j = 1; j <= m; j++)
+			aliveNum += (aliveCells[std::make_pair(i, j)] ? 1 : 0);
 }
 
 void PrintMap()
 {
-	system("cls");
+	// system("cls");
+	CLEARWINDOW;
 	std::cout << "Iteration num: " << iterNum << std::endl;
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j = 1; j <= m; j++)
-			std::cout.put(mp[i][j] ? 'O' : ' ');
+			std::cout.put(aliveCells[std::make_pair(i, j)] ? 'O' : ' ');
 		std::cout.put('\n');
 	}
-	std::cout << "Input ENTER to continue, or input Super+C to exit.";
-	char ch = std::cin.get();
+	if (argParser.get<bool>("useSleep") == false) {
+		std::cout << "Input ENTER to continue, or input Super+C to exit." << std::endl;
+		char ch = std::cin.get();
+	}
+	else {
+		int sleepTime = argParser.get<unsigned>("sleepTime");
+		SLEEP(sleepTime);
+	}
 }
